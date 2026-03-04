6298171e9d7de4813232f2330b04fdc7fec070c8
Adeeb Imam <adeebimam2511@gmail.com>
2026-03-04 16:41:25 +0000

    Customer delivery address + profile updates

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -11,6 +11,13 @@ class Profile(models.Model):
 
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     role = models.CharField(max_length=20, choices=Role.choices, default=Role.CUSTOMER)
-
+    business_name = models.CharField(max_length=255, blank = True)
+    contact_first_name = models.CharField(max_length=255, blank=True)
+    contact_last_name = models.CharField(max_length=255, blank=True)
+    phone = models.CharField(max_length=255, blank=True)
+    address = models.CharField(max_length=255,blank=True)
+    postcode = models.CharField(max_length=20, blank= True)
+    delivery_address = models.TextField(blank=True)
+    delivery_postcode = models.CharField(max_length=20, blank=True)
     def __str__(self):
         return f"{self.user.username} ({self.role})"
