378104d93d4b892d45d49360c740e086c5c3c193
Adeeb Imam <adeebimam2511@gmail.com>
2026-03-04 16:41:25 +0000

    Customer delivery address + profile updates

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("register/", views.register_view, name="register"),
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
+    path("register/producer/",views.producer_register_view, name="producer_register"),
+    path("profile/", views.profile_view, name="profile"),
 ]
